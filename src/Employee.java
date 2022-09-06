25c4ea5541f3802d0e6dc9443fd5087a28068a0b
janltu <klassenjan31@gmail.com>
2022-09-06 11:27:22 +0200

    Eingefügt welche Daten gespeichert werden sollen.

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,12 @@
 public class Employee {
+    private String name ,firstName, birthDate, department, particularity;
+    private double weight, income;
+    private int age, yearsOfService;
+    private char gender;
+
+    public fullName(String name, String firstName){
+
+
+    }
+
 }

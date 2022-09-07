e3b4a9209b9c0acb87e5f813ee535d376dbab642
DiegoEPH <giandieg@kkg-do.eu>
2022-09-07 15:27:38 +0200

    Main wurde zu Mainprogram unbenannt und Erster (test) Employee wurde eingefüht

---
+++ src/mainprogram.java
@@ -0,0 +1,10 @@
+public class mainprogram {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Lindenhorst", "Thomas");
+        System.out.println(e1.getName() + " , " + e1.getFirstname());
+
+    }
+
+}

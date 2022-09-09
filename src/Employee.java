459df676f7504890a1c820b57f95c5f74337e770
DiegoEPH <giandieg@kkg-do.eu>
2022-09-09 23:29:29 +0200

    Alle restlichen Daten (Gewicht, Geburtsdatum) wurden in Employee eingefügt

--- src/Employee.java
+++ src/Employee.java
@@ -4,9 +4,17 @@ public class Employee {
     private int age, yearsOfService;
     private char gender;
 
-    public Employee(String name, String firstName) {
+    public Employee(String name, String firstName, String birthDate, String department, String particularity, double weight, double income, int age, int yearsOfService, char gender) {
         this.name = name;
         this.firstName = firstName;
+        this.birthDate= birthDate;
+        this.department= department;
+        this.particularity= particularity;
+        this.weight= weight;
+        this.income= income;
+        this.age=age;
+        this.yearsOfService=yearsOfService;
+        this.gender=gender;
     }
 
         public String getName(){
@@ -14,10 +22,35 @@ public class Employee {
         }
         public String getFirstname(){
             return firstName;
+        }
+            public String getBirthdate(){
+                return birthDate;
+            }
+                public String getDepartment(){
+                    return department;
+                }
+                    public String getParticularity(){
+                        return particularity;
+                        }
+
+        public double getWeight(){
+        return weight;
+        }
+        public double getIncome(){
+        return income;
+        }
+            public int getAge(){
+                return age;
+            }
+                public int getYearsOfService(){
+                    return yearsOfService;
+                }
+                    public char getGender(){
+                        return gender;
+                    }
 
     }
 
 
 
 
-    }

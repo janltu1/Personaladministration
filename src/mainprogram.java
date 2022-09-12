90f6e244ccac0cb4b9073b935b6d411372425e3a
janltu <klasjanl@kkg-do.eu>
2022-09-12 18:39:37 +0200

    Daten der Mitarbeitern eingefügt.

--- src/mainprogram.java
+++ src/mainprogram.java
@@ -2,13 +2,13 @@ public class mainprogram {
 
     public static void main(String[] args) {
 
-        Employee e1 = new Employee("Nachname", "Vorname", "Datum","Bereich", "Besonderheiten", 0,0.0, 0, 0, 'M');
+        Employee e1 = new Employee("Klassen", "Jan Leon", "15.10.2006","IT-Sicherheit", "cool", 85,2500.0, 15, 1, 'M');
         System.out.println(e1.getName() + ", " + e1.getFirstname() + ", born at " + e1.getBirthdate() + ", " + e1.getAge() + " years old"+ ", gender="+ e1.getGender() + ", Weight=" + e1.getWeight() + ", Department=" + e1.getDepartment() + ", Particularity=" + e1.getParticularity() + ", Income=" + e1.getIncome() + ", years of service=" + e1.getYearsOfService());
 
-        Employee e2 = new Employee("Nachname", "Vorname", "Datum","Bereich", "Besonderheiten", 0,0.0, 0, 0, 'M');
+        Employee e2 = new Employee("Giancotti Orozco", "Diego", "09.07.2006","Verwaltung", "Sehr Schnell", 90,0.33, 16, 16, 'D');
         System.out.println(e2.getName() + ", " + e2.getFirstname() + ", born at " + e2.getBirthdate() + ", " + e2.getAge() + " years old"+ ", gender="+ e2.getGender() + ", Weight=" + e2.getWeight() + ", Department=" + e2.getDepartment() + ", Particularity=" + e2.getParticularity() + ", Income=" + e2.getIncome() + ", years of service=" + e2.getYearsOfService());
 
-        Employee e3 = new Employee("Nachname", "Vorname", "Datum","Bereich", "Besonderheiten", 0,0.0, 0, 0, 'M');
+        Employee e3 = new Employee("Mohammed", "Habib", "01.04.2006","Software Development", "Untergewichtig", 45,2000.02, 16, 1, 'M');
         System.out.println(e3.getName() + ", " + e3.getFirstname() + ", born at " + e3.getBirthdate() + ", " + e3.getAge() + " years old"+ ", gender="+ e3.getGender() + ", Weight=" + e3.getWeight() + ", Department=" + e3.getDepartment() + ", Particularity=" + e3.getParticularity() + ", Income=" + e3.getIncome() + ", years of service=" + e3.getYearsOfService());
 
     }
